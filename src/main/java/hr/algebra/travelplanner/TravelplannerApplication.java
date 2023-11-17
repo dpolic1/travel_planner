7fea95474bebc153c4bec73594a913e8e174afff
dpolic <dorijan.polic@comping.hr>
2023-11-17 11:29:37 +0100

    nothing

--- src/main/java/hr/algebra/travelplanner/TravelplannerApplication.java
+++ src/main/java/hr/algebra/travelplanner/TravelplannerApplication.java
@@ -3,6 +3,9 @@ package hr.algebra.travelplanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class TravelplannerApplication {
   public static void main(String[] args) {
